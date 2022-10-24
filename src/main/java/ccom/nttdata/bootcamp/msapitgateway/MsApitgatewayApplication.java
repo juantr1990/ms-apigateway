6aee13aa96b90496a69a9dfb26d7c80cb7ccae46
Juan Torres Romero <jtorrrom@emeal.nttdata.com>
2022-10-24 17:21:36 -0500

    base proyecto api gateway, sin eureka

---
+++ src/main/java/ccom/nttdata/bootcamp/msapitgateway/MsApitgatewayApplication.java
@@ -0,0 +1,14 @@
+package ccom.nttdata.bootcamp.msapitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class MsApitgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApitgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ccom/nttdata/bootcamp/msapitgateway/MsApitgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ccom.nttdata.bootcamp.msapitgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApitgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
